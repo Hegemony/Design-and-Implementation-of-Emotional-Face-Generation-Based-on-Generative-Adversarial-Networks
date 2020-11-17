5773364eecaf35fae68f5bf10b9fa0db3f3d5884
Boa Hancock <38655976+Hegemony@users.noreply.github.com>
2020-11-17 22:40:36 +0800

    Add files via upload

---
+++ data/data_loader.py
@@ -0,0 +1,55 @@
+import torch
+import os
+from PIL import Image
+import random
+import numpy as np
+import pickle
+import torchvision.transforms as transforms
+
+from .base_dataset import BaseDataset
+from .celeba import CelebADataset
+from .rafd import RaFDDataset  # 新建rafd数据集
+
+
+def create_dataloader(opt):
+    data_loader = DataLoader()
+    data_loader.initialize(opt)
+    return data_loader
+
+
+class DataLoader:
+    def name(self):
+        return self.dataset.name() + "_Loader"
+
+    def create_datase(self):
+        # specify which dataset to load here
+        loaded_dataset = os.path.basename(self.opt.data_root.strip('/')).lower()
+        if 'celeba' in loaded_dataset or 'emotion' in loaded_dataset:
+            dataset = CelebADataset()
+
+        elif 'rafd' in loaded_dataset:
+            dataset = RaFDDataset()
+
+        else:
+            dataset = BaseDataset()   # from .base_dataset import BaseDataset
+        dataset.initialize(self.opt)
+        return dataset
+
+    def initialize(self, opt):
+        self.opt = opt
+        self.dataset = self.create_datase()
+        self.dataloader = torch.utils.data.DataLoader(
+            self.dataset,
+            batch_size=opt.batch_size,
+            shuffle=not opt.serial_batches,
+            num_workers=int(opt.n_threads)
+        )
+
+    def __len__(self):
+        return min(len(self.dataset), self.opt.max_dataset_size)
+
+    def __iter__(self):
+        for i, data in enumerate(self.dataloader):
+            if i * self.opt.batch_size >= self.opt.max_dataset_size:
+                break
+            yield data
