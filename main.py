7a52b2319fa35f022e254aa219fe960d926ab36e
Boa Hancock <38655976+Hegemony@users.noreply.github.com>
2020-12-07 19:51:11 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from options import Options
+from solvers import create_solver
+
+
+
+
+if __name__ == '__main__':
+    opt = Options().parse()
+
+    solver = create_solver(opt)
+    solver.run_solver()
+
+    print('[THE END]')
